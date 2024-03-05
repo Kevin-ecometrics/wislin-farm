ffb003e137237fa6e00680b877303fbc2e24afba
Superuser <kevin@e-commetrics.com>
2024-03-05 15:08:22 -0800

    update languaje, content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  image: {
+    service: squooshImageService(),
+  },
+  integrations: [tailwind(), astroI18next()],
 });
